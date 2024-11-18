f096c1bd4119c1d90c1bbf4bdac09a2e4c6d4f11
heeyyeon <wjdgmldus28@gachon.ac.kr>
2024-11-19 02:36:53 +0900

    feat: 전역 스타일 관리 파일 설정

---
+++ src/style/theme.js
@@ -0,0 +1,26 @@
+const sizes = {
+  maxWidth: '480px',
+};
+
+// 색상, 폰트는 우리 서비스에 따라 바꿀 예정
+const colors = {
+  white: '#FFFFFF',
+  gray: '#94A3B8',
+  light_gray: '#F9F9F9',
+  yellow: '#FFD74F',
+  deep_gray: '#727272',
+  pink01: '#FFD6E7',
+  pink02: '#FF77B0',
+  main: '#FFA726',
+  light_main: '#E2E5FF',
+
+  //실제 디자인 컬러
+  orange: '#FFA726', //메인 컬러
+  caption2: '#858586', //그레이 2번
+  button: '#B6B6B7', //활성화 안 된 버튼 
+  black: '#323335', 
+};
+
+const theme = { colors, sizes };
+
+export default theme;
