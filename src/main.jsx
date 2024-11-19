fbf55a412d7f52a36b49ea8189bcd0cb37e790a7
chungyo <kimchungy98@gmail.com>
2024-11-19 14:40:52 +0900

    refactor: Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom';
+import router from './router/Router.jsx';
+import GlobalStyle from './style/GlobalStyle.js';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <RouterProvider router={router} />
+        </>
+    );
+}
+
+
+export default App;
